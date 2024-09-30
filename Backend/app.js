b1d627780792e4e70a7b337e64fdc177b83f8ae3
Erika Estrada <s-eetcat@hotmail.com>
2024-09-29 22:46:06 -0500

    Descripción de los cambios realizados

--- Backend/app.js
+++ Backend/app.js
@@ -3,12 +3,19 @@ const cors = require('cors');
 const dbconnect = require("./config");
 const app = express();
 
+// Middleware para manejar JSON y CORS
 app.use(express.json());
-app.use(cors({origin:'http://localhost:4200'}))
-app.use('/api/usuarios',require ('./routes/usuario.routes'));
-app.use('/api/gestion-empleados',require ('./routes/empleado.routes'));
-app.listen(3000, ()=>{
-    console.log("El Servidor está en el Puerto 3000")
-})
+app.use(cors({ origin: 'http://localhost:4200' }));
 
+// Definir rutas de la API
+app.use('/api/usuarios', require('./routes/usuario.routes'));
+app.use('/api/gestion-empleados', require('./routes/empleado.routes'));
+
+// Conectar a la base de datos
 dbconnect();
+
+// Asignar el puerto desde la variable de entorno (Vercel)
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`El servidor está en el puerto ${port}`);
+});
